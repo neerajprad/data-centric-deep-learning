c4c956aae7637d67c4d4544147b0e1d16bb23a25
Neeraj Pradhan <prad.neeraj@gmail.com>
2023-04-02 18:29:30 +0000

    add regression and directionality tests

--- course/week2/pipeline_project/src/tests/directionality.py
+++ course/week2/pipeline_project/src/tests/directionality.py
@@ -148,7 +148,7 @@ class MNISTDirectionalityTest(BaseTest):
       preds_raw = torch.argmax(logits_raw, dim=1)
       preds_transformed = torch.argmax(logits_transformed, dim=1)
 
-      batch_metric = 0  # store metric here
+      # batch_metric = 0  # store metric here
       # ================================
       # FILL ME OUT
       # 
@@ -169,6 +169,7 @@ class MNISTDirectionalityTest(BaseTest):
       # batch_metric: float (not torch.Tensor!)
       #   Metric computed on a minibatch
       # ================================
+      batch_metric = sum(preds_transformed == preds_raw).float() / len(preds_transformed)
       metric.append(batch_metric)
       pbar.update()
     pbar.close()
